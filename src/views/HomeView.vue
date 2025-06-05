78487b68299f5dd7232da59a879461d0e74c1535
Miquissefelix <fmiquissejose@gmail.com>
2025-06-05 13:25:47 +0200

    Adds Banner component and adjusts HomeView structure

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
-<script setup>
 
-</script>
 
 <template>
+     <Banner />
  <div class="main-container">
-     <h1>Home</h1>
+     <h1>Monte o seu burguer:</h1>
+     <p>Formulario</p>
  </div>
 </template>
+<script >
+import Banner from '../components/Banner.vue';
+export default{
+    name: 'HomeView',
+    components: {
+        Banner
+    },
+}
+</script>
