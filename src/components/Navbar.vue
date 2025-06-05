abc451916eedfd51c6377d44ff04f47f36ae27de
Miquissefelix <fmiquissejose@gmail.com>
2025-06-05 10:58:52 +0200

    Add initial components for Navbar, Footer, and PedidosView

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+   <header>
+   
+    <div class="">
+      <nav>
+        <router-link to="/">
+            <img src="" alt="">
+        </router-link>
+
+        <RouterLink to="/">
+            Home
+        </RouterLink>
+        <RouterLink to="/pedidos">
+            About
+        </RouterLink>
+      </nav>
+    </div>
+  </header>
+
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+
+}
+</script>
+
+<style>
+
+</style>
