3889e90f4500ca3840d5f4ffc4002f470a32a512
Miquissefelix <fmiquissejose@gmail.com>
2025-06-07 15:08:19 +0200

    Updates the Dashboard component to display the list of burgers and implement update and delete functionality

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -12,23 +12,26 @@
     </div>
     <!--  -->
     <div class="burguer-table-rows">
-        <div class="burguer-table-row">
-            <div class="order-number">1</div>
-            <div>joao</div>
-            <div>pao de trigo</div>
-            <div>maminha</div>
+        <div class="burguer-table-row" v-for="burger in burgers" :key="burger.id">
+            <div class="order-number">{{ burger.id }}</div>
+            <div>{{ burger.nome }}</div>
+            <div>{{ burger.pao }}</div>
+            <div>{{ burger.carne }}</div>
             <div>
                 <ul>
-                    <li>Salame</li>
-                    <li>Tomate</li>
+                    <li v-for="(opcional,index) in burger.opcionais" :key="index">{{ opcional }}</li>
+                   
                 </ul>
             </div>
 
             <div>
-                <select name="status" class="status">
+                <select name="status" class="status" @change="updateBurger($event,burger.id)">
                     <option value="">Selecione </option>
+                     <option v-for="s in status" :key="s.id" :value="s.tipo" :selected="burger.status == s.tipo">
+                        {{ s.tipo }} 
+                    </option>
                 </select>
-                <button class="delete-btn">Cancelar</button>
+                <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
             </div>
         </div>
     </div>
@@ -39,6 +42,60 @@
 <script>
 export default {
 name: 'Dashboard',
+  data(){
+    return{
+       burgers:null,
+       burger_id:null,
+       status:[]
+    }
+  },
+  methods:{
+    async getBurger(){
+      const req= await fetch('http://localhost:3000/burgers');
+      const data=await req.json();
+
+      this.burgers=data;
+
+    //   console.log(this.burgers);
+      
+    // recover status
+    this.getStatus();
+    },
+    
+    async getStatus(){
+    const req = await fetch('http://localhost:3000/status');
+    const data = await req.json();
+    this.status = data;
+
+ 
+  },
+  async deleteBurger(id){
+    const req = await fetch(`http://localhost:3000/burgers/${id}`,{
+        method: 'DELETE'
+    })
+    const res = await req.json();
+
+     this.getBurger(); // refresh 
+  },
+  async updateBurger(e,id){
+    const option=e.target.value;
+    const dataJson=JSON.stringify({status:option});
+
+    const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+        method: 'PATCH',
+        headers: {'Content-Type': 'application/json'},
+        body: dataJson
+    });
+
+    //res- response from the server
+    const res = await req.json();
+  }
+  },
+
+ 
+  mounted(){
+    this.getBurger();
+  }
 }
 </script>
 
